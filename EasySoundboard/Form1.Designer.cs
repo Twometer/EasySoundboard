3e7a0cebc9619498ee2d8ff997783080b92f4db3
Twometer <twometer@outlook.de>
2021-04-17 18:15:23 +0200

    Microphone relaying

--- EasySoundboard/Form1.Designer.cs
+++ EasySoundboard/Form1.Designer.cs
@@ -29,21 +29,25 @@ namespace EasySoundboard
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbInputDev = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cbOutputDev = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cbInputDev
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(108, 15);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(190, 23);
-            this.comboBox1.TabIndex = 0;
+            this.cbInputDev.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbInputDev.FormattingEnabled = true;
+            this.cbInputDev.Location = new System.Drawing.Point(108, 15);
+            this.cbInputDev.Name = "cbInputDev";
+            this.cbInputDev.Size = new System.Drawing.Size(266, 23);
+            this.cbInputDev.TabIndex = 0;
             // 
             // label1
             // 
@@ -63,46 +67,81 @@ namespace EasySoundboard
             this.label2.TabIndex = 2;
             this.label2.Text = "Output Device:";
             // 
-            // comboBox2
+            // cbOutputDev
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(108, 54);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(190, 23);
-            this.comboBox2.TabIndex = 3;
+            this.cbOutputDev.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbOutputDev.FormattingEnabled = true;
+            this.cbOutputDev.Location = new System.Drawing.Point(108, 54);
+            this.cbOutputDev.Name = "cbOutputDev";
+            this.cbOutputDev.Size = new System.Drawing.Size(266, 23);
+            this.cbOutputDev.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(202, 94);
+            this.button1.Location = new System.Drawing.Point(278, 83);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "Open Devices";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.Location = new System.Drawing.Point(19, 162);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(279, 192);
+            this.groupBox1.Size = new System.Drawing.Size(355, 192);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Sound Management";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(19, 360);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(355, 124);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Input Effects";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(214, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(122, 87);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 19);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Monitoring";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(322, 378);
+            this.ClientSize = new System.Drawing.Size(386, 529);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.cbOutputDev);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbInputDev);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
             this.Text = "EasySoundboard";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,12 +149,15 @@ namespace EasySoundboard
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbInputDev;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cbOutputDev;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- EasySoundboard/Form1.cs
+++ EasySoundboard/Form1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CSCore;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.SoundOut;
+using CSCore.Streams;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +17,77 @@ namespace EasySoundboard
 {
     public partial class Form1 : Form
     {
+        private SimpleMixer mixer;
+        private WasapiCapture soundIn;
+        private WasapiOut soundOut;
+        private WasapiOut monitoringOut;
+
         public Form1()
         {
             InitializeComponent();
+            ReloadDevices();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cbInputDev.SelectedItem != null && cbOutputDev.SelectedItem != null)
+            {
+                cbInputDev.Enabled = cbOutputDev.Enabled = button1.Enabled = button2.Enabled = checkBox1.Enabled = false;
+
+                soundIn = new WasapiCapture(true, AudioClientShareMode.Shared, 30);
+                soundIn.Device = (MMDevice)cbInputDev.SelectedItem;
+                soundIn.Initialize();
+                soundIn.Start();
+
+                mixer = new SimpleMixer(soundIn.WaveFormat.Channels, soundIn.WaveFormat.SampleRate);
+
+                var waveSource = new SoundInSource(soundIn) { FillWithZeros = true };
+                mixer.AddSource(waveSource.ToSampleSource());
+
+                var mixedSource = mixer.ToWaveSource();
+
+                soundOut = new WasapiOut();
+                soundOut.Device = (MMDevice)cbOutputDev.SelectedItem;
+                soundOut.Initialize(mixedSource);
+                soundOut.Play();
+
+                if (checkBox1.Checked)
+                {
+                    var monitorSource = new SoundInSource(soundIn) { FillWithZeros = true };
+                    monitoringOut = new WasapiOut();
+                    monitoringOut.Initialize(monitorSource);
+                    monitoringOut.Play();
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReloadDevices();
+        }
+
+        private void ReloadDevices()
+        {
+            cbInputDev.Items.Clear();
+            cbOutputDev.Items.Clear();
+            foreach (var device in MMDeviceEnumerator.EnumerateDevices(DataFlow.All))
+            {
+                if (device.DeviceState != DeviceState.Active)
+                    continue;
+
+                if (device.DataFlow == DataFlow.Capture && !cbInputDev.Items.Contains(device))
+                    cbInputDev.Items.Add(device);
+                else if (!cbOutputDev.Items.Contains(device))
+                    cbOutputDev.Items.Add(device);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            soundIn?.Dispose();
+            soundOut?.Dispose();
+            monitoringOut?.Dispose();
         }
     }
 }
